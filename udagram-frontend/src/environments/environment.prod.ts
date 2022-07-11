6f081dc799acf5878b39f81095a45250fe0d3f02
lethanhduy228 <lethanhduy228@gmail.com>
2022-07-11 16:55:48 +0700

    fix bug at service user

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af4ea3846ffd64d34a5b38d2a96a1bd4-805935356.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
